5d02552c4fbb464079d31c1bfef4602b048456d1
Pierce Thompson <insprill@gmail.com>
2022-09-05 18:59:39 -0400

    Add signing and nexusPublishing tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -80,3 +80,17 @@ publishing {
         }
     }
 }
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications)
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+        }
+    }
+}
