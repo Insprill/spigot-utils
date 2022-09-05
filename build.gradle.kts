59c85eb729d2ccd43d965deb06193d81693036d1
Pierce Thompson <insprill@gmail.com>
2022-09-05 19:04:24 -0400

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("java")
     id("signing")
     id("maven-publish")
-    id("io.freefair.lombok") version "6.4.3.1"
+    id("io.freefair.lombok") version "6.5.1"
     id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
 }
 
@@ -18,9 +18,9 @@ dependencies {
     compileOnly("org.jetbrains:annotations:23.0.0")
     compileOnly("org.spigotmc:spigot-api:1.19.2-R0.1-SNAPSHOT")
 
-    testImplementation("com.github.seeseemelk:MockBukkit-v1.18:2.22.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testImplementation("com.github.seeseemelk:MockBukkit-v1.18:2.85.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
 }
 
 tasks {
